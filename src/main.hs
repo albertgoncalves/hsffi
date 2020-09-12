4765dbe0dc9a0c6e28037f6cc2baf82cbf4c8724
Albert Goncalves <goncalves.albert@gmail.com>
2020-09-12 19:31:45 -0400

    more prints

--- src/main.hs
+++ src/main.hs
@@ -1,14 +1,14 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
-import Control.Monad (replicateM_)
+import Control.Monad (replicateM, replicateM_)
 import Foreign.C.Types (CFloat (..), CULong (..))
 
 foreign import ccall "set_seed" c_set_seed :: CULong -> CULong -> IO ()
 
-foreign import ccall "get_random_f32" c_get_random_f32 :: IO CFloat
-
 setSeed :: Int -> Int -> IO ()
 setSeed s i = c_set_seed (fromIntegral s) (fromIntegral i)
+
+foreign import ccall "get_random_f32" c_get_random_f32 :: IO CFloat
 
 getRandom :: IO Float
 getRandom = realToFrac <$> c_get_random_f32
@@ -32,11 +32,25 @@ shuffle xs = do
   (l, r) <- partitionM (\_ -> (< 0.5) <$> getRandom) xs
   (++) <$> shuffle l <*> shuffle r
 
+printIndent :: String -> IO ()
+printIndent x = do
+  putStr "\t"
+  putStrLn x
+
 main :: IO ()
 main = do
   setSeed 0 0
-  replicateM_ 10 $ getRandom >>= print
-  partitionM (\_ -> (< 0.5) <$> getRandom) xs >>= print
-  replicateM_ 3 $ shuffle xs >>= print
+  putStrLn "\nreplicateM_ 10 $ getRandom"
+  replicateM_ 10 $ getRandom >>= printIndent . show
+  putStrLn $ "\nreplicateM " ++ show n ++ " getRandom"
+  xs' <- replicateM n getRandom
+  printIndent $ "mean : " ++ show (sum xs' / (fromIntegral n :: Float))
+  printIndent $ "max  : " ++ show (maximum xs')
+  printIndent $ "min  : " ++ show (minimum xs')
+  putStrLn $ "\npartitionM (\\_ -> (< 0.5) <$> getRandom) " ++ show xs
+  partitionM (\_ -> (< 0.5) <$> getRandom) xs >>= printIndent . show
+  putStrLn $ "\nreplicateM_ 3 $ shuffle " ++ show xs
+  replicateM_ 3 $ shuffle xs >>= printIndent . show
   where
     xs = [1 .. 10 :: Int]
+    n = 100000 :: Int
